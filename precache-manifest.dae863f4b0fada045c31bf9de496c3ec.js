de584cd3458cbb14cc36d1a0e076784412dd4256
Mateus Silva <a.mateuslol@gmail.com>
2020-07-25 02:51:23 -0300

    site

---
+++ precache-manifest.dae863f4b0fada045c31bf9de496c3ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61f1a5b6ae2bc4a88b57eb8951d59b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6902bd231c0da3472dbe",
+    "url": "/static/css/main.a5c72695.chunk.css"
+  },
+  {
+    "revision": "b245a211f793c585c349",
+    "url": "/static/js/2.bdae109a.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.bdae109a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6902bd231c0da3472dbe",
+    "url": "/static/js/main.e478fb02.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  },
+  {
+    "revision": "abd2ba69bb6bf8469130b703115dd2a2",
+    "url": "/static/media/home-background.abd2ba69.svg"
+  },
+  {
+    "revision": "26434f1f8d4600c2289a91f27afe85a8",
+    "url": "/static/media/logo.26434f1f.svg"
+  }
+]);
